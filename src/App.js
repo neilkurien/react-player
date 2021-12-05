e2522261f2724ee0eb8dadfd12c9bdbdc61692e9
Neil <nkurien97@gmail.com>
2021-12-05 20:28:53 +0530

    connected data to app, currentSong displays works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, {useState} from "react";
 import "./styles/app.scss";
+
 //Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+
 //Import Data
 import data from "./data";
 
 function App() {
   //State
   const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
