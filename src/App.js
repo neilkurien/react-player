88dfa12b0bc4aff3a0ccaf1364dbe91cfb7549d8
Neil <nkurien97@gmail.com>
2021-12-05 18:34:36 +0530

    added data.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, {useState} from "react";
 import "./styles/app.scss";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//Import Data
+import data from "./data";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Music Player</h1>
-      </header>
+      <Song />
+      <Player />
     </div>
   );
 }
