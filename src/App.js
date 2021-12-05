9d598b8e43ca96575c6e1deb97bae9569a8036fc
Neil <nkurien97@gmail.com>
2021-12-05 17:29:35 +0530

    added new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Music Player</h1>
       </header>
     </div>
   );
