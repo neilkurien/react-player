4b794fa2a64ae34b95fc6855e6359e21d79784b8
Neil <nkurien97@gmail.com>
2021-12-26 20:35:34 +0530

    custom input slider with gradient

--- src/components/Player.js
+++ src/components/Player.js
@@ -75,11 +75,21 @@ const Player = ({
     playAudio(isPlaying, audioRef);
   };
 
+  //Add styles
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  };
+
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <div className="track">
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+          }}
+          className="track"
+        >
           <input
             min={0}
             max={songInfo.duration || 0}
@@ -87,7 +97,7 @@ const Player = ({
             onChange={dragHandler}
             type="range"
           />
-          <div className="animate-track"></div>
+          <div style={trackAnim} className="animate-track"></div>
         </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
